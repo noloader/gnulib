79957c0d7b631d745d305268be088508dfffb9af
Jeffrey Walton <noloader@gmail.com>
2020-03-28 17:21:32 -0400

    Travis commit

--- ci_tests/test_modules.sh
+++ ci_tests/test_modules.sh
@@ -6,21 +6,26 @@ test_dir=$(dirname $(pwd))/gnulib_test
 rm -rf "$test_dir"
 mkdir -p "$test_dir"
 
+result_file=$(pwd)/result.txt
 module_list=$(find modules -name '*tests' | sed -e 's|^modules/||g' -e 's|-tests$||g' | sort | head -n 32)
+
+# Eventual return code
+failed_rests=0
 
 for module in $module_list
 do
     columns=$(tput cols)
     header="****************************************"
-    printf "%*s\n" $(((${#header}+$columns)/2)) "$header"
-    printf "%*s\n" $(((${#module}+$columns)/2)) "$module"
-    printf "%*s\n" $(((${#header}+$columns)/2)) "$header"
+    printf "%*s\n" $(((${#header}+$columns)/2)) "$header" | tee -a "$result_file"
+    printf "%*s\n" $(((${#module}+$columns)/2)) "$module" | tee -a "$result_file"
+    printf "%*s\n" $(((${#header}+$columns)/2)) "$header" | tee -a "$result_file"
 
     cd "$this_dir" || exit 1
 
     if ! ./gnulib-tool --create-testdir --dir="$test_dir/$module" "$module" 1>/dev/null;
     then
-        echo "Failed to prepare $module"
+        echo "Failed to prepare $module" | tee -a "$result_file"
+        failed_rests=$((failed_rests+1))
         continue
     fi
 
@@ -28,21 +33,27 @@ do
 
     if ! ./configure;
     then
-        echo "Failed to configure $module"
+        echo "Failed to configure $module" | tee -a "$result_file"
+        failed_rests=$((failed_rests+1))
         continue
     fi
 
-    if ! make;
+    # Travis offers two cores
+    if ! make -j 3;
     then
-        echo "Failed to make $module"
+        echo "Failed to make $module" | tee -a "$result_file"
+        failed_rests=$((failed_rests+1))
         continue
     fi
 
     if ! make check;
     then
-        echo "Failed to test $module"
+        echo "Failed to test $module" | tee -a "$result_file"
+        failed_rests=$((failed_rests+1))
         continue
     fi
 
-    echo "Finished testing $module"
+    echo "Tested $module OK" | tee -a "$result_file"
 done
+
+exit "$failed_rests"
