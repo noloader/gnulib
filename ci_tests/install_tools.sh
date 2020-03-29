12f47721c02fd18dec86c1b48d6e4c57371047a5
Jeffrey Walton <noloader@gmail.com>
2020-03-28 21:07:54 -0400

    Cirrus commit

--- ci_tests/install_tools.sh
+++ ci_tests/install_tools.sh
@@ -5,8 +5,8 @@ echo "Installing tools..."
 if [ "$TRAVIS_OS_NAME" = "linux" ]
 then
 
+    # This should not fail
     sudo apt-get -y install --no-install-recommends make clang gcc autoconf automake autopoint libtool pkg-config 1>/dev/null || true
-
     # This may fail on older Linux distros
     sudo apt-get -y install --no-install-recommends clang-8 &>/dev/null || true
 
@@ -15,7 +15,9 @@ fi
 if [ "$TRAVIS_OS_NAME" = "osx" ]
 then
 
-    brew update  || true
-    brew install make clang gcc autoconf automake autopoint libtool pkg-config 1>/dev/null || true
+    # Homebrew will fail in unexpected ways, like returning failure when it is up to date.
+    brew update || true
+    # And it will fail here, too, if there is a package update available???
+    brew install make clang autoconf automake autopoint libtool pkg-config 1>/dev/null || true
 
 fi
