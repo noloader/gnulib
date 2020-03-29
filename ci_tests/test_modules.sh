89c0bcf3ab29b6412a0fa231efda537d67083b89
Jeffrey Walton <noloader@gmail.com>
2020-03-29 02:53:10 -0400

    Update test_modules.sh

--- ci_tests/test_modules.sh
+++ ci_tests/test_modules.sh
@@ -23,23 +23,17 @@ else
     MAKE=make
 fi
 
-# Blue is light blue
-color_red='\033[0;31m'
-color_blue='\033[1;34m'
-color_green='\033[0;32m'
-color_end='\033[0m'
-
 for module in $module_list
 do
     header="****************************************"
-    printf "%s\n" "${color_blue}${header}${color_end}" | tee -a "$result_file"
-    printf "%s\n" "${color_blue}${module}${color_end}" | tee -a "$result_file"
+    printf "%s\n" "${header}" | tee -a "$result_file"
+    printf "%s\n" "${module}" | tee -a "$result_file"
 
     cd "$this_dir" || exit 1
 
     if ! ./gnulib-tool --create-testdir --dir="$test_dir/$module" "$module" 1>/dev/null;
     then
-        printf "%s\n" "${color_red}Failed to prepare $module${color_end}" | tee -a "$result_file"
+        printf "%s\n" "Failed to prepare $module" | tee -a "$result_file"
         failed_tests=$((failed_tests+1))
         continue
     fi
@@ -48,7 +42,7 @@ do
 
     if ! ./configure;
     then
-        printf "%s\n" "${color_red}Failed to configure $module${color_end}" | tee -a "$result_file"
+        printf "%s\n" "Failed to configure $module" | tee -a "$result_file"
         failed_tests=$((failed_tests+1))
         cat config.log
         continue
@@ -57,19 +51,19 @@ do
     # Travis offers two cores
     if ! ${MAKE} -j 3;
     then
-        printf "%s\n" "${color_red}Failed to make $module${color_end}" | tee -a "$result_file"
+        printf "%s\n" "Failed to make $module" | tee -a "$result_file"
         failed_tests=$((failed_tests+1))
         continue
     fi
 
     if ! ${MAKE} check;
     then
-        printf "%s\n" "${color_red}Failed to test $module${color_end}" | tee -a "$result_file"
+        printf "%s\n" "Failed to test $module" | tee -a "$result_file"
         failed_tests=$((failed_tests+1))
         continue
     fi
 
-    printf "%s\n" "${color_green}Tested $module OK${color_end}" | tee -a "$result_file"
+    printf "%s\n" "${color_green}Tested $module OK" | tee -a "$result_file"
 done
 
 exit "$failed_tests"
